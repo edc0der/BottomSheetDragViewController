c134ecb2bf7f4f73d7c9a31ff37ae706c14b38fc
Eduard Moya <eduard.moya@gmail.com>
2018-12-21 17:39:51 -0300

    Initial Commit

---
+++ BottomDragPresentation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BottomDragPresentation
+//
+//  Created by Eduard Moya on 21/12/2018.
+//  Copyright © 2018 Eduard Moya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
